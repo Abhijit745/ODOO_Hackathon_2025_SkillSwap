3c5a32fbbd958342b4ee5b755b03d7516c92712c
SatabdiSatyapragnyaSwain <swainsatabdi20@gmail.com>
2025-07-12 15:18:24 +0530

    initial commit

---
+++ ESL_Api/DataAccessLayer/DAL.cs
@@ -0,0 +1,26 @@
+﻿using Dapper;
+using SkillSwap.IDataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace SkillSwap.DataAccessLayer
+{
+    public class DAL : IDAL
+    {
+        //public List<DashboardData> DashboardShowData()
+        //{
+        //    List<DashboardData> data = null;
+        //    using (IDbConnection db = new SqlConnection(Appsetting.ConnectionString))
+        //    {
+        //        db.Open();
+        //        var _params = new DynamicParameters();
+        //        data = db.Query<DashboardData>(Appsetting.SQLQueryCommand.GetTripDetails, _params, commandType: CommandType.StoredProcedure, commandTimeout: 180).ToList();
+        //    }
+        //    return data;
+        //}
+    }
+}
---
+++ ESL_Api/Models/UserDetailsModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SkillSwap.Models
+{
+    public class GetUserDetailsModel
+    {
+        public Guid userID { get; set; }
+        public string userMailID { get; set; }
+        public string userPassword { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string profilePhoto { get; set; }
+        public string userLocation { get; set; }
+        public string userAvailability { get; set; }
+        public string profileStatus { get; set; }
+        public List<userSkills> wantedSkills { get; set; }
+        public List<userSkills> offeredSkills { get; set; }
+    }
+
+    public class userSkills
+    {
+        public int skillID { get; set; }
+        public string skillName { get; set; }
+        public string SkillType { get; set; }
+    }
+
+    public class CreateUserDetailsModel
+    {
+        public string ID { get; set; }
+        public string userEmail { get; set; }
+        public string userPassword { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string profilePhoto { get; set; }
+        public string userLocation { get; set; }
+        public string userAvailability { get; set; }
+        public string profileStatus { get; set; }
+        public string action { get; set; }
+        public List<SkillModel> wantedSkill { get; set; }
+        public List<SkillModel> offeredSkill { get; set; }
+    }
+
+    public class SkillModel
+    {
+        public int skillID { get; set; }
+    }
+}
